af22cd8058cc503b25d8a2d5d8061736937dc706
Will Robinson <will@oznetnerd.com>
2019-08-09 11:02:04 +1000

    Update

--- Packer/config/gitlab-setup.sh
+++ Packer/config/gitlab-setup.sh
@@ -6,3 +6,8 @@ sudo docker pull gitlab/gitlab-ce:latest
 sudo docker pull gitlab/gitlab-runner:alpine
 sudo docker pull docker:stable
 sudo docker pull registry:2
+mkdir -p /home/ec2-user/gitlab-config
+cd  /home/ec2-user/gitlab-config
+wget https://raw.githubusercontent.com/OzNetNerd/Packer-Gitlab/master/Packer/config/Makefile
+wget https://raw.githubusercontent.com/OzNetNerd/Packer-Gitlab/master/Packer/config/docker-compose.yml
+sudo chown -R ec2-user:ec2-user /home/ec2-user
